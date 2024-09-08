a83bba923e6255cb67f24385bf37eaddfce1ce8a
RubanDenis <140437029+RubanDenis@users.noreply.github.com>
2024-09-08 13:21:32 +0300

    Адаптивные сетки (#9)
    * Сетка главной страницы
    
    * Сетка главной и каталога
    
    * Стилизация сетки главной страницы
    
    * Стилизация сетки каталога
    
    * Сетка
    
    * Сетка главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const navigation = document.querySelector('.navigation');
+const navigationToggle = document.querySelector('.navigation__toggle');
+const siteList = document.querySelector('.site-list');
+
+navigationToggle.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--close');
+  navigation.classList.toggle('navigation--open');
+  siteList.classList.toggle('site-list--open');
+});
