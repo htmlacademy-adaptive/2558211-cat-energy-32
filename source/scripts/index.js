36bd33018404ffbc76b4647261495602e4c51ac3
RubanDenis <140437029+RubanDenis@users.noreply.github.com>
2024-09-18 20:53:11 +0300

    Адаптивные декоративные элементы (#10)
    * Адаптация сеток
    
    * Адаптация сеток
    
    * Декоративные элементы
    
    * Доработка главной страницы
    
    * Доработка страницы каталога
    
    * Правки стилей
    
    * Дополнительные стили
    
    * Удалил лишние файлы стилей

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,8 @@ const navigation = document.querySelector('.navigation');
 const navigationToggle = document.querySelector('.navigation__toggle');
 const siteList = document.querySelector('.site-list');
 
+navigation.classList.remove('navigation--nojs');
+
 navigationToggle.addEventListener('click', () => {
   navigation.classList.toggle('navigation--close');
   navigation.classList.toggle('navigation--open');
